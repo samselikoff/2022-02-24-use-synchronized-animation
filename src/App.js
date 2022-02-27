3e45d73cc8f802008af05780d3b4f05418a5473c
Sam Selikoff <sam.selikoff@gmail.com>
2022-02-26 21:03:30 -0500

    End of video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 
 export default function App() {
   let [count, setCount] = useState(1);
+  let [key, setKey] = useState(0);
 
   return (
     <div className="p-8">
@@ -18,18 +19,58 @@ export default function App() {
         >
           Remove
         </button>
+        <button
+          onClick={() => setKey((key) => key + 1)}
+          className="px-2 py-1 border border-zinc-300 active:bg-zinc-100"
+        >
+          Rerender
+        </button>
       </div>
 
       <div className="mt-4 space-y-6">
         {[...Array(count).keys()].map((el, i) => (
-          <Spinner key={i} />
+          <Spinner key={`${i}-${key}`} />
         ))}
       </div>
     </div>
   );
 }
 
+let stashedTime;
+
+function useSynchronizedAnimation(animationName) {
+  let ref = useRef();
+
+  useLayoutEffect(() => {
+    let animations = document
+      .getAnimations()
+      .filter((animation) => animation.animationName === animationName);
+
+    let myAnimation = animations.find(
+      (animation) => animation.effect.target === ref.current
+    );
+
+    if (myAnimation === animations[0] && stashedTime) {
+      myAnimation.currentTime = stashedTime;
+    }
+
+    if (myAnimation !== animations[0]) {
+      myAnimation.currentTime = animations[0].currentTime;
+    }
+
+    return () => {
+      if (myAnimation === animations[0]) {
+        stashedTime = myAnimation.currentTime;
+      }
+    };
+  }, [animationName]);
+
+  return ref;
+}
+
 function Spinner() {
+  let ref = useSynchronizedAnimation("spin");
+
   return (
     <svg
       className="w-10 h-10 mx-auto text-sky-400"
@@ -47,6 +88,7 @@ function Spinner() {
         d="M15 0C6.716 0 0 6.716 0 15h4.5C4.5 9.201 9.201 4.5 15 4.5V0z"
         fill="currentColor"
         className="origin-center text-sky-400 animate-spin"
+        ref={ref}
       />
     </svg>
   );
